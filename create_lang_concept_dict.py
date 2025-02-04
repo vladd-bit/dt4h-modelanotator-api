eccf5c0f306b2377e60c953a66f56b1a66314a8b
vladd-bit <vlad.a.dinu@gmail.com>
2025-02-04 12:17:32 +0000

    Changed ref to entities

--- create_lang_concept_dict.py
+++ create_lang_concept_dict.py
@@ -1,32 +1,32 @@
 import csv
 import os
 
-swedish_entities = []
+entities = []
 
 language = os.getenv("DICTIONARY_FILE", "english")
 
 language_source_files_path = "./lang/"
 
 with open(os.path.join(language_source_files_path, f"{language}_variables.csv"),
-           "r") as file:
+          "r") as file:
     reader = csv.reader(file)
     for row in reader: 
-        swedish_entities.append([row[1], "", row[0], "DT4H", "2024-06-01"])
+        entities.append([row[1], "", row[0], "DT4H", "2024-06-01"])
 
 with open(os.path.join(language_source_files_path, f"{language}_medication.csv"),
           "r") as file:
     reader = csv.reader(file)
     for row in reader:
-        swedish_entities.append([row[1], "Medication", row[0], "DT4H", "2024-06-01"])
+        entities.append([row[1], "Medication", row[0], "DT4H", "2024-06-01"])
 
-with open("./english_entities.csv", "r") as file:
+with open("./{language}_entities.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader: 
-        for idx in range(len(swedish_entities)):
-            if swedish_entities[idx][2] == row[2]:
-                if swedish_entities[idx][1] == "":
-                    swedish_entities[idx][1] = row[1]
+        for idx in range(len(entities)):
+            if entities[idx][2] == row[2]:
+                if entities[idx][1] == "":
+                    entities[idx][1] = row[1]
 
 with open(f"./{language}_entities.csv", 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerows(swedish_entities)
+    writer.writerows(entities)
